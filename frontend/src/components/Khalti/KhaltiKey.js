d685f8a1fc6bb66937f3f36f27c824dcefeb2e09
Roshan2058 <roshanshriwastav924@gmail.com>
2022-09-21 21:46:44 +0545

    first commit

---
+++ frontend/src/components/Khalti/KhaltiKey.js
@@ -0,0 +1,6 @@
+const myKey = {
+    publicTestKey: "test_public_key_d8be3a43f4354f52abb32515c0c2038e",
+    secretKey: "Key test_secret_key_6093f22766784c1788f0a48cbd611d2e",
+  };
+  
+  export default myKey;
